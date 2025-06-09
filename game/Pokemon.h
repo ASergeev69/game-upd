8f442f9cd88718e122b7d70cf706186c43c1880c
Sergeev Alexander <jason57marsov@gmail.com>
2025-06-09 16:25:44 +0300

    little new features for poke

--- game/Pokemon.h
+++ game/Pokemon.h
@@ -21,6 +21,23 @@ private:
 	vector<Move> moves;
 public:
 
+    int getExpToNextLevel() const {
+        return level * 50; 
+    }
+
+    void addExp(int amount) {
+        exp += amount;
+
+        while (exp >= getExpToNextLevel()) {
+            exp -= getExpToNextLevel();
+            level++;
+            maxHP += 10;
+            moves[0].damage += 0.5;
+            moves[1].damage += 0.5;
+            HP += 10;
+        }
+    }
+
     Move getMove(int index) { return moves.at(index); }
     void addMove(const Move& move) { moves.push_back(move); }
     void clearMoves() { moves.clear(); }
@@ -28,8 +45,8 @@ public:
     void setTexture(Texture2D tex_) { tex = tex_; }
     const Texture2D& getTexture() const { return tex; }
 
-    void setName(const std::string& name_) { name = name_; }
-    const std::string& getName() const { return name; }
+    void setName(const string& name_) { name = name_; }
+    const string& getName() const { return name; }
 
     void setType(pokemonType type_) { type = type_; }
     pokemonType getType() const { return type; }
@@ -46,8 +63,8 @@ public:
     void setLevel(int value) { level = value; }
     int getLevel() const { return level; }
 
-    void setMoves(const std::vector<Move>& m) { moves = m; }
-    const std::vector<Move>& getMoves() const { return moves; }
+    void setMoves(const vector<Move>& m) { moves = m; }
+    const vector<Move>& getMoves() const { return moves; }
 
     void takeDamage(float dmg) { HP = std::max(0.0, HP - dmg*1.0); }
 };
